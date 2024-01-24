f048acac061a5b640537e072acea36dc03c83957
Nebel2613 <nebel2613@gmail.com>
2024-01-24 21:29:35 +0100

    swap reset btn and grid places

---
+++ script.js
@@ -0,0 +1,6 @@
+const pixel = document.createElement("div");
+const btn = document.querySelector("button");
+btn.addEventListener("click", ()=>{
+    const INPUT = parseInt(prompt("How many pixels is there to be on the side (Ex: 16 for a 16x16 grid)", "16"));
+    
+})
