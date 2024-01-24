179dd314e7bedb3ae7e2961a6d1bcfad6f8ca065
Nebel2613 <nebel2613@gmail.com>
2024-01-24 22:17:42 +0100

    can now create the grid

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const pixel = document.createElement("div");
 const btn = document.querySelector("button");
-btn.addEventListener("click", ()=>{
-    const INPUT = parseInt(prompt("How many pixels is there to be on the side (Ex: 16 for a 16x16 grid)", "16"));
-    
+let input = 16;
+create(input);
+
+function create(input) {
+    let SIZE = 100/input;
+    //Destroy and create container
+    const old = document.querySelector(".container");
+    const body = document.querySelector("body");
+    body.removeChild(old);
+    const container = document.createElement("div");
+    container.classList.add("container")
+    body.appendChild(container);
+    //fill it up
+    for (let i = 0; i < input * input; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.style.cssText = `width: ${SIZE}%; height : ${SIZE}%`;
+    container.appendChild(pixel);
+}
+}
+//event listener on button
+btn.addEventListener("click", function () {
+    input = parseInt(prompt("Enter the number of pixels on the side of the grid(Ex: 16 for a 16*16 grid", "16"));
+    create(input);
 })
